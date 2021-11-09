80330419851a845664b51796a6e86033551a4bbf
Yuan Cao <yuan.cao.email@gmail.com>
2021-11-09 13:38:20 -0500

    Add name and title

--- example packages/myAdd/R/myAdd.R
+++ example packages/myAdd/R/myAdd.R
@@ -2,6 +2,8 @@
 #'
 #' @export
 #' @return 0L
+#' @name zero
+#' @title zero
 #' @examples
 #' zero() # Captures nothing?
 zero <- function() {
@@ -14,6 +16,8 @@ zero <- function() {
 #' @param a A number.
 #' @param b A number.
 #' @return The sum of \code{a} and \code{b}.
+#' @name add
+#' @name title
 #' @examples
 #' add(2L, 3L) # Captures 2L and 3L?
 add <- function(a, b) {
